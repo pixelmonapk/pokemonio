3ebeeb19fad0405519b9b24f3dfbc7281735e4fe
mwear3robloxandroid <mitchellwears@gmail.com>
2016-12-01 11:58:19 +0000

    Create game.C

---
+++ player/parts/game.C
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+// This is not an editor script.
+public class MyPlayer : MonoBehaviour {
+    public int armor = 1;
+    public int damage = 1;
+    public GameObject gun;
+
+    void Update () {
+	// Update logic here...
+    }
+}
